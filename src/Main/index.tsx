c13531d0b1c527db37926a7ab57f7a5b09a8613b
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-03-27 15:18:20 -0300

    :sparkles: feat: added splash screen structure

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,10 +1,30 @@
-import { Container } from "./style";
+import {
+  Container,
+  CategoriesContainer,
+  MenuContainer,
+  Footer,
+  FooterContainer,
+} from "./style";
 import { Header } from "../components/Header";
+
+import { Categories } from "../components/Categories";
+import { Menu } from "../components/Menu";
 
 export const Main = () => {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <>
+      <Container>
+        <Header />
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+        <MenuContainer>
+          <Menu></Menu>
+        </MenuContainer>
+      </Container>
+      <Footer>
+        <FooterContainer></FooterContainer>
+      </Footer>
+    </>
   );
 };
