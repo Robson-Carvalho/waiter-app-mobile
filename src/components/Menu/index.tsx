e09e4956d552da4661cad949114b6fe618ea2f62
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-03-27 22:21:02 -0300

    :sparkles: feat: added menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,16 @@
 import { FlatList } from "react-native";
-import { Product, Image, ProductDetails } from "./styles";
+import {
+  Product,
+  ProductImage,
+  ProductDetails,
+  Separator,
+  AddToCartButton,
+} from "./styles";
 import { Text } from "../Text/Text";
+
+import { PlusCircle } from "../Icons/PlusCircle";
+
+import { formatCurrency } from "../../utils/formartCurrency";
 
 import { products } from "../../mocks/products";
 
@@ -11,19 +21,28 @@ export const Menu = () => {
       contentContainerStyle={{ paddingHorizontal: 24 }}
       data={products}
       keyExtractor={(products) => products._id}
+      ItemSeparatorComponent={Separator}
       renderItem={({ item: product }) => (
         <Product>
-          <Image
+          <ProductImage
             source={{
               uri: `http://192.168.0.105:3001/${product.imagePath}`,
             }}
           />
 
           <ProductDetails>
-            <Text>{product.name}</Text>
-            <Text>{product.description}</Text>
-            <Text>{product.price}</Text>
+            <Text weight="600">{product.name}</Text>
+            <Text color="#666" size={14} style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text color="#333" weight="600" size={14}>
+              {formatCurrency(product.price)}
+            </Text>
           </ProductDetails>
+
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
         </Product>
       )}
     />
--- src/mocks/products.ts
+++ src/mocks/products.ts
@@ -2,7 +2,7 @@ export const products = [
   {
     _id: "6372e276a381106c0f854cb3",
     name: "Coca cola",
-    description: "Coca cola lata geladinha topzera",
+    description: "Coca cola lata gelada",
     imagePath: "1679605949703-coca-cola.png",
     price: 7,
     ingredients: [],
@@ -10,7 +10,7 @@ export const products = [
   {
     _id: "6372e276a381106c0f854cbf33",
     name: "Coca cola",
-    description: "Coca cola lata geladinha topzera",
+    description: "Coca cola lata gelada",
     imagePath: "1679605949703-coca-cola.png",
     price: 7,
     ingredients: [],
@@ -18,7 +18,7 @@ export const products = [
   {
     _id: "6372e276a38113f06c0f854cb3",
     name: "Coca cola",
-    description: "Coca cola lata geladinha topzera",
+    description: "Coca cola lata gelada",
     imagePath: "1679605949703-coca-cola.png",
     price: 7,
     ingredients: [],
@@ -26,7 +26,23 @@ export const products = [
   {
     _id: "6372e276a3f3f81106c0f854cb3",
     name: "Coca cola",
-    description: "Coca cola lata geladinha topzera",
+    description: "Coca cola lata gelada",
+    imagePath: "1679605949703-coca-cola.png",
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: "6372e276fa3f3f81106c0f854cb3",
+    name: "Coca cola",
+    description: "Coca cola lata gelada",
+    imagePath: "1679605949703-coca-cola.png",
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: "6372e276a3ff3f81106c0f854cb3",
+    name: "Coca cola",
+    description: "Coca cola lata gelada",
     imagePath: "1679605949703-coca-cola.png",
     price: 7,
     ingredients: [],
